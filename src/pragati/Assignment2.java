531f385b8addd73c46d3caaabcd2a088d967fd7a
Pragati Sawant <sawantpragati410@gmail.com>
2022-06-21 07:27:43 +0530

    Ass2_pragati

--- src/pragati/Assignment2.java
+++ src/pragati/Assignment2.java
@@ -48,7 +48,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Assignment2 {
  private WebDriver driver;
   void setProperty(String url)
