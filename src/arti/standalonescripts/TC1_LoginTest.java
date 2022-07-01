970c77b4d47014c39f97fb97847d491000711634
Arti Shinde <arti.shinde89@gmail.com>
2022-07-01 23:34:47 +0530

    updated

--- src/arti/standalonescripts/TC1_LoginTest.java
+++ src/arti/standalonescripts/TC1_LoginTest.java
@@ -17,11 +17,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class TC1_LoginTest {
 
 	private WebDriver driver;
-	
+	@Test
 	public void loginTest() {
 	
 		System.out.println("STEP - Launch browser");
